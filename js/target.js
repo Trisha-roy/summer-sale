8e46960df03e1f1c714af8f745e6f59d79400476
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-21 02:38:05 +0600

    added Cards

--- js/target.js
+++ js/target.js
@@ -1,10 +1,41 @@
-document.getElementById('card-1').addEventListener('click',function(){
-    const name=document.getElementById('card-name-1');
-    const nameValue=name.innerText;
-    console.log(nameValue);
+// document.getElementById('card-1').addEventListener('click',function(){
+//     const name=document.getElementById('card-name-1');
+//     const nameValue=name.innerText;
+//     console.log(nameValue);
 
-    const card=document.getElementById('product-list')
-    card.innerText=nameValue
+//     const card=document.getElementById('list-1')
+//     card.innerText=nameValue;
+
+//     const price=document.getElementById('price-1');
+//     const priceValue=price.innerText;
+//     console.log(priceValue);
+
+//     const cardPrice=document.getElementById('product-list-1')
+//     cardPrice.innerText=priceValue;
+
+
 
     
-})
+// })
+// document.getElementById('card-2').addEventListener('click',function(){
+//     const name_1=document.getElementById('card-name-2');
+//     const name_1Value=name_1.innerText;
+//     console.log(name_1Value);
+
+//     const card_1=document.getElementById('product-list')
+//     card_1.innerText=name_1Value;
+
+//     const price_1=document.getElementById('price-2');
+//     const priceValue_1=price_1.innerText;
+//     console.log(priceValue_1);
+
+//     const cardPrice_1=document.getElementById('product-list-1')
+//     cardPrice_1.innerText=priceValue_1;
+
+
+
+    
+// })
+function handleClick(target){
+    console.log(target.parentNode.childNodes[10].innerText)
+}
