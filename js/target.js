e8b17524a69d5ace81d86e5f85ecfed316a7061b
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-21 20:37:21 +0600

    Add Price section

--- js/target.js
+++ js/target.js
@@ -1,60 +1,39 @@
-document.getElementById('card-1').addEventListener('click',function(){
-    const name=document.getElementById('card-name-1');
-    const nameValue=name.innerText;
-    console.log(nameValue);
- 
+let total=0;
+
+function handleClick(target){
+    const itemName=target.parentNode.childNodes[5].childNodes[3].childNodes[3].innerText;
+
     //append child
-    const card=document.getElementById('list-1')
-    const li=document.createElement('li')
-    li.innerText=nameValue;
-    card.appendChild(li);
+    const listItem=document.getElementById('list-1');
+    const li=document.createElement('li');
+    li.innerText=itemName;
+    listItem.appendChild(li);
 
-    const price=document.getElementById('price-1');
-    const priceValue=price.innerText;
-    console.log(priceValue);
+    //price section
+    const price=target.parentNode.childNodes[5].childNodes[3].childNodes[5].innerText.split(" ")[0];
+    console.log(price)
+    total=parseInt(total)+parseInt(price);
+    const totalPrice=document.getElementById('totalPrice');
+    totalPrice.innerText=total;
 
-    const cardPrice=document.getElementById('totalPrice')
-    cardPrice.innerText=priceValue;
+    const purchaseButton=document.getElementById('purchase-btn');
+
+    if(total>0){
+        purchaseButton.removeAttribute('disabled')
+    }else{
+        purchaseButton.setAttribute('disabled')
+    }
+
+    const applyBtn=document.getElementById('apply-btn');
+    const applyField=document.getElementById
     
-})
-// document.getElementById('card-2').addEventListener('click',function(){
-//     const name_1=document.getElementById('card-name-2');
-//     const name_1Value=name_1.innerText;
-//     console.log(name_1Value);
-
-//     const card_1=document.getElementById('product-list')
-//     card_1.innerText=name_1Value;
-
-//     const price_1=document.getElementById('price-2');
-//     const priceValue_1=price_1.innerText;
-//     console.log(priceValue_1);
-
-//     const cardPrice_1=document.getElementById('product-list-1')
-//     cardPrice_1.innerText=priceValue_1;
-
-
-
-    
-// })
-
-//card-2
-document.getElementById('card-2').addEventListener('click',function(){
-    const name=document.getElementById('card-name-2');
-    const nameValue=name.innerText;
-    console.log(nameValue);
- 
-    //append child
-    const card=document.getElementById('list-1')
-    const li=document.createElement('li')
-    li.innerText=nameValue;
-    card.appendChild(li);
-
-    const price2=document.getElementById('price-2');
-    const priceValue2=price2.innerText;
-    console.log(priceValue2);
-
-    const cardPrice2=document.getElementById('totalPrice')
-    cardPrice2.innerText=priceValue2;
-    
-})
-console.log(price2)
+    if(total>200){
+        applyBtn.removeAttribute('disabled')
+    }else{
+        applyBtn.setAttribute('disabled')
+    }
+    //input field=
+    const inputField=document.getElementById('input-field');
+    const input=inputField.value;
+    console.log(input)
+}
